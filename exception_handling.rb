88b5357e04f6ce78cbb87a8682ad804da55c0ebf
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-05 20:15:32 +0530

    Exceptions with extra information.

--- exception_handling.rb
+++ exception_handling.rb
@@ -19,9 +19,17 @@ class ExceptionHandling
     end
     puts 'I\'m just a later placeholder which won\'t be execcuted as rescue returns'
   end
+
+  def capture_with_message(item_to_be_divided)
+    item_to_be_divided / 0
+  rescue ZeroDivisionError => e
+    puts "Error message #{e.message}"
+    puts "Error backtrace #{e.backtrace}"
+  end
 end
 
 obj = ExceptionHandling.new
 # obj.unhandled_divide_by_zero(450) # Raises ZeroDivisionError
 obj.handled_divide_by_zero(450)
 obj.handled_divide_by_zero_and_more(54)
+obj.capture_with_message(54)
